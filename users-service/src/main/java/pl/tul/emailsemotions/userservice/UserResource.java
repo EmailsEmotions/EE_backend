3833453905a687ff92baed82204e44fe1b7cbd9c
Bartłomiej Jencz <bartek.jencz@wp.pl>
2021-05-19 22:19:55 +0200

    Working basic users service

---
+++ users-service/src/main/java/pl/tul/emailsemotions/userservice/UserResource.java
@@ -0,0 +1,35 @@
+package pl.tul.emailsemotions.userservice;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.tul.emailsemotions.userservice.model.User;
+import pl.tul.emailsemotions.userservice.model.UserRepository;
+
+import java.sql.Date;
+import java.time.LocalTime;
+import java.util.List;
+
+@RequestMapping("/users")
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class UserResource {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping(value = "/{userId}")
+    public User findUser(@PathVariable("userId") int userId) {
+
+        return new User(1L,"username","email@abc.pl","de");
+//        return userRepository.findById(userId);
+    }
+}
