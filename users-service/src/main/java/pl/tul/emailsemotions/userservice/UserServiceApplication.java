d5da7e57c4b6f575ebfb1f0f2694d04ebcc3d084
Bartłomiej Jencz <bartek.jencz@wp.pl>
2021-05-26 11:37:34 +0200

    Added uuid for app

--- users-service/src/main/java/pl/tul/emailsemotions/userservice/UserServiceApplication.java
+++ users-service/src/main/java/pl/tul/emailsemotions/userservice/UserServiceApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.util.UUID;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
 
     public static void main(String[] args) {
+        System.setProperty("app_uuid", UUID.randomUUID().toString());
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
