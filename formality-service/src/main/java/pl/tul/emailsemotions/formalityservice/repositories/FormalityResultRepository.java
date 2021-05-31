2504d357d97c72ce02f16379fc55b53ea9e22470
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2021-05-31 17:45:57 +0200

    Formality result repository.

--- formality-service/src/main/java/pl/tul/emailsemotions/formalityservice/repositories/FormalityResultRepository.java
+++ formality-service/src/main/java/pl/tul/emailsemotions/formalityservice/repositories/FormalityResultRepository.java
@@ -3,6 +3,6 @@ package pl.tul.emailsemotions.formalityservice.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.tul.emailsemotions.formalityservice.model.FormalityResult;
 
-public interface FormalityResultRepository extends JpaRepository<FormalityResult, Integer> {
+public interface FormalityResultRepository extends JpaRepository<FormalityResult, Long> {
 }
 
