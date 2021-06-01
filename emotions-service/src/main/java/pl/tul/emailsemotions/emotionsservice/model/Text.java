14af005ebe2d8d7c7dc6f7085befbeda89e7ed8d
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2021-06-01 16:27:56 +0200

    Emotions service model: text and emotions result.

---
+++ emotions-service/src/main/java/pl/tul/emailsemotions/emotionsservice/model/Text.java
@@ -0,0 +1,38 @@
+package pl.tul.emailsemotions.emotionsservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.LinkedList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "texts")
+public class Text {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "text")
+    @NotEmpty
+    private String text;
+
+    @OneToMany(
+        cascade = CascadeType.ALL,
+        orphanRemoval = true,
+        fetch = FetchType.EAGER
+    )
+    @JoinColumn(name = "text_id")
+    private List<EmotionsResult> emotionsResults = new LinkedList<>();
+
+    public void addEmotionsResult(EmotionsResult emotionsResult) {
+        emotionsResults.add(emotionsResult);
+    }
+}
