56f6eb1f9b4163f99a7d1ca2a0bd89df899a83eb
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2021-06-04 17:59:54 +0200

    Formality service: getting results and evaluation based on user id implemented.

--- formality-service/src/main/java/pl/tul/emailsemotions/formalityservice/services/EvaluationService.java
+++ formality-service/src/main/java/pl/tul/emailsemotions/formalityservice/services/EvaluationService.java
@@ -6,19 +6,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.tul.emailsemotions.formalityservice.model.FormalityEvaluation;
 import pl.tul.emailsemotions.formalityservice.repositories.FormalityEvaluationRepository;
-import pl.tul.emailsemotions.formalityservice.model.Text;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class EvaluationService {
     private final FormalityEvaluationRepository formalityEvaluationRepository;
-    private final TextService textService;
 
     public FormalityEvaluation add(FormalityEvaluation formalityEvaluation) throws NotFoundException {
         formalityEvaluationRepository.save(formalityEvaluation);
-        Text text = textService.get(formalityEvaluation.getTextId());
-        text.addFormalityEvaluation(formalityEvaluation);
         return formalityEvaluation;
+    }
+
+    public List<FormalityEvaluation> getAllByTextId(Long textId) {
+        return formalityEvaluationRepository.findAllByTextId(textId);
     }
 }
