c063bb0d7f32e4f05aa166b7564796efdd8cd9d3
Bartłomiej Jencz <bartek.jencz@wp.pl>
2021-06-07 19:21:52 +0200

    Improved log in UserService

--- users-service/src/main/java/pl/tul/emailsemotions/userservice/UserService.java
+++ users-service/src/main/java/pl/tul/emailsemotions/userservice/UserService.java
@@ -17,15 +17,18 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User add(User user) {
+        log.info("Adding user to database: " + user.toString());
         return userRepository.save(user);
     }
 
     public List<User> getAll() {
+        log.info("Fetching all users");
         return userRepository.findAll();
     }
 
     public User get(Long id) throws EntityNotFoundException {
         if (userRepository.findById(id).isPresent()) {
+            log.info("User with id: " + id + " found");
             return userRepository.findById(id).get();
         }
         log.error("User with id: " + id + " not found");
@@ -34,6 +37,7 @@ public class UserService {
 
     public User findByUsername(String username) {
         if (userRepository.findByUsername(username).isPresent()) {
+            log.info("User with username: " + username + " found");
             return userRepository.findByUsername(username).get();
         }
         log.error("User with username: " + username + " not found");
@@ -45,6 +49,7 @@ public class UserService {
             User user = this.get(id);
             user.setActive(status);
             userRepository.save(user);
+            log.info("Changed active status for userid: "+id.toString()+" to: "+ status.toString());
             return true;
         } catch (EntityNotFoundException ex) {
             log.error("User with id: " + id + " not found. Cannot change active status");
@@ -57,6 +62,7 @@ public class UserService {
             User user = this.get(id);
             user.setConfirmed(status);
             userRepository.save(user);
+            log.info("Changed confirmed status for userid: "+id.toString()+" to: "+ status.toString());
             return true;
         } catch (EntityNotFoundException ex) {
             log.error("User with id: " + id + " not found. Cannot change confirmed status");
@@ -69,6 +75,7 @@ public class UserService {
             User user = this.get(id);
             user.setAccountType(accountType);
             userRepository.save(user);
+            log.info("Changed account type for userid: "+id.toString()+" to: "+ accountType.toString());
             return true;
         } catch (EntityNotFoundException ex) {
             log.error("User with id: " + id + " not found. Cannot change account type");
