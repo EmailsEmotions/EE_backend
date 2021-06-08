2bfec53d7eec37d37b9e6b2804e6ec6ec674b994
Bartłomiej Jencz <bartek.jencz@wp.pl>
2021-06-08 12:03:28 +0200

    Added logstash to admin-server

--- admin-server/src/main/java/pl/tul/emailsemotions/admin/AdminServerApplication.java
+++ admin-server/src/main/java/pl/tul/emailsemotions/admin/AdminServerApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.util.UUID;
+
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
 public class AdminServerApplication {
 
     public static void main(String[] args) {
+        System.setProperty("app_uuid", UUID.randomUUID().toString());
         SpringApplication.run(AdminServerApplication.class, args);
     }
 
