cd6f6423561044fc451ce7f4ec8305677be109fc
Bartłomiej Jencz <bartek.jencz@wp.pl>
2021-06-14 12:39:05 +0200

    Enabled feign clients

--- emotions-service/src/main/java/pl/tul/emailsemotions/emotionsservice/EmotionsServiceApplication.java
+++ emotions-service/src/main/java/pl/tul/emailsemotions/emotionsservice/EmotionsServiceApplication.java
@@ -3,11 +3,13 @@ package pl.tul.emailsemotions.emotionsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.UUID;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class EmotionsServiceApplication {
 
     public static void main(String[] args) {
