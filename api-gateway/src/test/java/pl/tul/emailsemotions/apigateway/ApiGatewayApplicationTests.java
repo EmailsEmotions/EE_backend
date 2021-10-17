be2f05b4c67a54f522aafb77a8ca90a718d21d36
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2021-10-17 11:24:21 +0200

    Added api gateway swagger tests.

--- api-gateway/src/test/java/pl/tul/emailsemotions/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/pl/tul/emailsemotions/apigateway/ApiGatewayApplicationTests.java
@@ -2,12 +2,18 @@ package pl.tul.emailsemotions.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
     @Test
-    void contextLoads() {
+    void swaggerConfigTest() {
+        SwaggerConfig swaggerConfig = new SwaggerConfig();
+        Docket api = swaggerConfig.api();
+        assertThat(api.getDocumentationType().toString()).isEqualTo("openApi:3.0");
+        assertThat(api.getGroupName()).isEqualTo("default");
     }
-
 }
