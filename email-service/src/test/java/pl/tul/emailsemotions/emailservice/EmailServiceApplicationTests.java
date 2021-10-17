3b57b656f964618eacd9a015a782c1b92a4e88db
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2021-10-17 11:45:40 +0200

    Email regex test in emails service added.

--- email-service/src/test/java/pl/tul/emailsemotions/emailservice/EmailServiceApplicationTests.java
+++ email-service/src/test/java/pl/tul/emailsemotions/emailservice/EmailServiceApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.FileCopyUtils;
+import pl.tul.emailsemotions.emailservice.utils.EmailsEmotionsUtilites;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.io.*;
@@ -21,15 +22,28 @@ class EmailServiceApplicationTests {
     @Value("classpath:templates/template.html")
     private Resource resource;
 
+    public static final String EMAIL_TEST_CORRECT_1 = "mateusz.walczak@p.lodz.pl";
+    public static final String EMAIL_TEST_CORRECT_2 = "bartlomiej.jencz123@p.lodz.pl";
+    public static final String EMAIL_TEST_WRONG_1 = "konrad.kajszczak@www";
+    public static final String EMAIL_TEST_WRONG_2 = "konrad.kajszczak.p.lodz.pl";
+
     @Test
     void mailTemplateTest() {
-            try (Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
-                String template = FileCopyUtils.copyToString(reader);
-                assertThat(template).contains("EmailsEmotions Message");
-                assertThat(template).contains("Wiadomość wysłana z systemu EmailsEmotions");
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
+            String template = FileCopyUtils.copyToString(reader);
+            assertThat(template).contains("EmailsEmotions Message");
+            assertThat(template).contains("Wiadomość wysłana z systemu EmailsEmotions");
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    @Test
+    void emailRegexTest() {
+        assertThat(EMAIL_TEST_CORRECT_1.matches(EmailsEmotionsUtilites.EMAIL_REGEX)).isEqualTo(true);
+        assertThat(EMAIL_TEST_CORRECT_2.matches(EmailsEmotionsUtilites.EMAIL_REGEX)).isEqualTo(true);
+        assertThat(EMAIL_TEST_WRONG_1.matches(EmailsEmotionsUtilites.EMAIL_REGEX)).isEqualTo(false);
+        assertThat(EMAIL_TEST_WRONG_2.matches(EmailsEmotionsUtilites.EMAIL_REGEX)).isEqualTo(false);
     }
 
     @Test
