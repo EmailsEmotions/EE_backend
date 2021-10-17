9036cdb3bfda8f6cb4c122dbcccde6cafc139f9c
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2021-10-17 11:20:42 +0200

    Fix of formality service javax validation error: added missing dependency to pom.xml

--- formality-service/src/main/java/pl/tul/emailsemotions/formalityservice/clients/model/User.java
+++ formality-service/src/main/java/pl/tul/emailsemotions/formalityservice/clients/model/User.java
@@ -4,10 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
